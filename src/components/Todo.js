9cbfc29427aa199347d573b8c5df68c57c653b0d
lkopi246 <asharma.bhwn@gmail.com>
2020-04-02 14:27:20 +0530

    react boiler with csimple component

---
+++ src/components/Todo.js
@@ -0,0 +1,4 @@
+import React from "react";
+const Todo = () => <div>games</div>;
+
+export default Todo;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "public")
+  }
+};
